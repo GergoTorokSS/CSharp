34a441becec6e12660ef4f1599c591245a879d9f
GergoTorokSS <113582530+GergoTorokSS@users.noreply.github.com>
2022-09-15 09:15:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Osztaly
+{
+    public static int valami = 0;
+    public Osztaly()
+    {
+        valami++;
+        Console.WriteLine("Az objektumok száma: {0}",valami);
+    }
+    ~Osztaly()
+    {
+        valami--;
+        Console.WriteLine("Az objektumok száma: {0}", valami);
+    }
+   
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Osztaly Obj = new Osztaly();
+        }
+    }
+}
